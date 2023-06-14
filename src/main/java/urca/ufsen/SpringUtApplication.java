4eddb0e6603996a4d9db55a79928f9191b39b342
abdiallo <abdiallo@gainde2000.sn>
2023-06-14 18:48:18 +0200

    V1 test

---
+++ src/main/java/urca/ufsen/SpringUtApplication.java
@@ -0,0 +1,13 @@
+package urca.ufsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/urca/ufsen/SpringUtApplicationTests.java
@@ -0,0 +1,13 @@
+package urca.ufsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
