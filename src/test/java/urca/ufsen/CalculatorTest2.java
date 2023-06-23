a81bc721b9babd0ac1530a2fe5d6ff5a1ddde3d1
BassirouD <33847402+BassirouD@users.noreply.github.com>
2023-06-23 06:03:32 +0200

    Update CalculatorTest2.java

--- src/test/java/urca/ufsen/CalculatorTest2.java
+++ src/test/java/urca/ufsen/CalculatorTest2.java
@@ -1,27 +1,20 @@
 package urca.ufsen;
+
 import org.junit.jupiter.api.Test;
 import urca.ufsen.entities.Calculator;
-import urca.ufsen.entities.MathUtils;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest2 {
     @Test
     public void testAdd() {
-        int result = MathUtils.add(2, 3);
-        assertEquals(0, result);
-    }
-    @Test
-    public void testSubtract() {
-        int result = MathUtils.subtract(5, 3);
-        assertEquals(2, result);
-    }
-    @Test
-    public void testCalculatorAdd() {
         Calculator calculator = new Calculator();
         int result = calculator.add(2, 3);
         assertEquals(5, result);
     }
+
     @Test
-    public void testCalculatorSubtract() {
+    public void testSubtract() {
         Calculator calculator = new Calculator();
         int result = calculator.subtract(5, 3);
         assertEquals(2, result);
